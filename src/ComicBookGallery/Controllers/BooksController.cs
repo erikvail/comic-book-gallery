3e5e830948234827a90e7457be2ba604b3f1d438
Erik Gabor <ge.ahun@hotmail.com>
2020-03-10 23:23:27 +0200

    Book controller add

---
+++ src/ComicBookGallery/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class BooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+            {
+                //return new RedirectResult("/");
+                return Redirect("/");
+            }
+            return new ContentResult() {
+                Content = "Hello from Book Controller on " + DateTime.Today.DayOfWeek
+            };
+        }
+    }
+}
